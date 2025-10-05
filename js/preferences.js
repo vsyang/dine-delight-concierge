cfa13d5dd7fc5c9584afc3bfc6dd9476f84aa54d
vsyang <yang.vanessa85@gmail.com>
2025-10-04 18:31:31 -0600

    improving UI

--- js/preferences.js
+++ js/preferences.js
@@ -1,15 +1,15 @@
-import { getUberEats } from "./restaurant.js";
-import { searchMovie } from "./movie.js";
+// import { getUberEats } from "./restaurant.js";
+// import { searchMovie } from "./movie.js";
 
 
-getUberEats({
-  address: "1401 Alberni Street",
-  resName: "LE COQ FRIT",
-  country: "Canada",
-  city: "Vancouver",
-})
-  .then((data) => {
-    console.log("UberEats via function:", data);
-    // e.g., document.querySelector("#ubereats").textContent = JSON.stringify(data, null, 2);
-  })
-  .catch(console.error);
+// getUberEats({
+//   address: "1401 Alberni Street",
+//   resName: "LE COQ FRIT",
+//   country: "Canada",
+//   city: "Vancouver",
+// })
+//   .then((data) => {
+//     console.log("UberEats via function:", data);
+//     // e.g., document.querySelector("#ubereats").textContent = JSON.stringify(data, null, 2);
+//   })
+//   .catch(console.error);
